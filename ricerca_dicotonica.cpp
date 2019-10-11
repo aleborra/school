570ed3c4f8daf78f1ebc7428a1c4f4ecec727b49
Arianoxx <32774517+francitato@users.noreply.github.com>
2019-10-11 09:17:58 +0200

    ricerca_dicatonica: fix this shit

--- ricerca_dicotonica.cpp
+++ ricerca_dicotonica.cpp
@@ -2,123 +2,98 @@
 #define MAX_VET 100
 
 
+void scambia_int(int *ptra,int *ptrb) 
+{
+	
+	int dep;
+	dep=*ptra;
+	*ptra=*ptrb;
+	*ptrb=dep;
+}
+
+
 void carica_vet(int *vet, int n)
 {
 	int i;
 	
-	for(i=0;i<n;i++)
-	{
-		printf("inserisci un numero \n");
-		scanf("%d", &vet[i]);
-	}
-	
+	for(i=0;i<n;i++) {
+		printf("Inserisci un numero:\n");
+		scanf("%d",&vet[i]);
+	}	
 }
 
-
-void stampa_vet(int *vet, int n)
+void stampa_vet(int *vet,int n)
 {
 	int i;
 	
-	for(i=0;i<n;i++)
-	{
-		printf("%d ", vet[i]);	
+	for(i=0;i<n;i++) {
+		
+		printf("\n%d",vet[i]);
 	}
-	printf("\n");
 }
 
-/*
-void cerca(int *vet,int n, int el)			//ricerca dicotonica si sua con un solo valore e vettore ordinato
+void bubble_sort (int *vet,int n) {
+	
+	int i,j,scambio;
+	i=(n-1);
+	scambio=1;
+	
+	while(scambio==1&&i>0)        //va anche solo scambio
+	{
+		scambio=0;
+		for(j=0;j<i;j++) 
+		{
+			if(vet[j]>vet[j+1])
+			{
+			 	scambia_int(&vet[j],&vet[j+1]);
+				scambio=1;	
+			}
+		}
+		i--;
+	}
+}
+
+int cerca(int *vet,int n,int el)     //ricerca dicotomica
 {
-	int i, j, med, trovato;
-	i=0;									//i inizio
-	j=n-1;									//j fine
-	trovato= 1;
+	int i,j,med,trovato;
+	
+	i=0;
+	j=n-1;     
+	trovato=0;
 	
 	while(!trovato&&i<=j)
 	{
 		med=(i+j)/2;
-		if(vet[med==el])
-			
+		if(vet[med]==el)
 			trovato=1;
-		
 		else
-			
-			if(vet[med]>el)					//se > uso j se < uso i
-			{
-				j=med-1;
-			}
+			if(vet[med]>el)    //elemento sta a sinistra
+				j=med-1;	
 			else
-			{
 				i=med+1;
-			}
 	}
-
 	if(trovato)
-	{
-		printf("posizione %d\n", med);
-	}
-	else
-	{
-		printf("elemento non trovato");
-	}
-}
-*/
-
-void cerca(int *vet,int n, int el)		
-{
-	int i, j, med, trovato;
-	i=0;									
-	j=n-1;									
-	trovato= 1;
-	
-	while(!trovato&&i<=j)
-	{
-		med=(i+j)/2;
-		if(vet[med==el])
-			
-			trovato=1;
-		
-		else
-			
-			if(vet[med]>el)				
-			{
-				j=med-1;
-			}
-			else
-			{
-				i=med+1;
-			}
-	}
-
-	if(trovato)
-	{
 		return med;
-	}
 	else
-	{
 		return -1;
-	}
 }
-
-int main(){
 	
-	int len, numeri[MAX_VET], ord;
-	
-	printf("quanti numeri vuoi inserire \n");
-	scanf("%d",&len);
-
-	carica_vet(numeri, len);
-	stampa_vet(numeri, len);
-	ris=cerca(numeri, len);
-	
+	int main () {
+		int vet[MAX_VET],n,el,ris;
+		printf("Inserire dimensione vettore:\n");
+		scanf("%d",&n);
+		
+		carica_vet(vet,n);
+		bubble_sort(vet,n);
+		stampa_vet(vet,n);
+		printf("Quale elemento vuoi cercare:\n");
+		scanf("%d",&el);
+		ris=cerca(vet,n,el);
+		
 	if(ris!=-1)
-	{
-		printf("posizione %d\n", ris);
-	}
+		printf("Elemento trovato in posizione %d\n",ris);
 	else
-	{
-		printf("elemento non trovato");
+		printf("Elemento non trovato\n");
+	
 	}
-	
-	
-}
+
